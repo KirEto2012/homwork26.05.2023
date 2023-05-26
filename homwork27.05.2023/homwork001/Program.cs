f0a822e5905c6d44b44a25b1e49b9374cfa4ffba
«liya» <ilialisov8410@gmail.com>
2023-05-27 02:57:11 +0700

    homwork001.27.05,2023

---
+++ homwork27.05.2023/homwork001/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+Console.WriteLine("Впишыте чесло a");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Впишыте чесло b");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine("число под буквой a=" + numberA + " больше чем число под буквлй b=" + numberB);
+}
+else
+{
+    Console.WriteLine("число под буквой b=" + numberB + " больше чем число под буквлй a=" + numberA);
+}
