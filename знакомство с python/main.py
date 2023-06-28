67eed9558392f86319a792026e6806c4f19930ad
«liya» <ilialisov8410@gmail.com>
2023-06-28 23:14:09 +0700

    знакомство с python

---
+++ знакомство с python/main.py
@@ -0,0 +1,57 @@
+
+print('ввидите первыую строку: ')
+a =int(input())
+b =int(input('ввидите второе число: '))
+print(a, '+', b, '=', a + b)
+
+# c = 1
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+# a = 5.89956
+# b = 6.556551
+# print(round(a*b, 3))
+
+# iter = 2
+# iter += 3
+# print(iter)
+
+# a = 1 > 4
+# print(a)
+# a = 1 < 4 and 5 > 2
+# print(a)
+
+
+# username = input('Ввидите имя: ')
+# if username == 'Маша':
+#     print('Ура этоже Маша!!!!!')
+# elif username == 'Марина':
+#     print('Я так ждал Вас, Марина!!!!')
+# elif username == 'Ильнар':
+#     print('hi Ильнар')
+# else:
+#     print('hello,', username)
+
+# a = 'qwerty'
+
+# for i in a:
+#     print(i)
+
+# line = ""
+# for i in range(5):
+#     line = ""
+#     for j in range(5):
+#         line += "*"
+#     print(line)
+
+# text = 'Сешь ещё этих Мягких фронцузких булочек'
+# print(len(text))
+# print('ещё' in text)
+# print(text.lower())
+# print(text.upper())
+# print(text.replace('ещё', 'ЕЩЁ'))
+
+
